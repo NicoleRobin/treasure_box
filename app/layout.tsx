0719f295d0cab1cc6579112a2fa2c981b2b6733b
robin zhang <lit050528@gmail.com>
2024-11-27 21:45:02 +0800

    Update - daily commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,12 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import React from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
